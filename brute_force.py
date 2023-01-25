fe995a06aceb2062d90cfe28ea90143d3d111c17
shimmer4 <ahujadevesh1@gmail.com>
2023-01-25 21:49:32 +0530

    brute force with worst path too

--- brute_force.py
+++ brute_force.py
@@ -34,6 +34,39 @@ def bruteForce(cities):
     return minTour, bestPath
 
 
+
+
+def bruteForceWorst(cities):
+    """
+    brute force but for the worst path possible instead of the best
+    (THIS CODE IS NOT TO BE ADDED IN FINAL PROJECT)
+    """
+
+    permutations = permute( [i for i in range(len(cities))] )
+    print(f"total {len(permutations)} to compute.")
+    
+    minTour, bestPath = 0, None
+    
+    progress = 0
+    for path in permutations:
+        progress += 1
+        tourLength = 0
+
+        for i in range(len(path)):
+            city, next_city = cities[ path[i] ], cities[ path[(i+1) % len(path)] ]
+            tourLength += city.distance(next_city)
+        
+        if tourLength > minTour:
+            bestPath = path
+            minTour = tourLength
+        
+        if progress % 10000 == 0: print(f"{str(progress / len(permutations) * 100)[:5]}% permutations computed.", end='\r')
+    
+    return minTour, bestPath
+
+
+
+
 def getPathCoords(path, cities):
     x_list, y_list = [], []
 
@@ -66,7 +99,7 @@ def permute(nums):
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    N = 10  # dont go beyong 9
+    N = 8  # dont go beyong 9
     
     cities = generateCities(N)
     minTour, bestPath = bruteForce(cities)
@@ -74,4 +107,10 @@ if __name__ == "__main__":
     print(f"time taken for {N} cities -> {str(time.perf_counter() - start_time)[:7]} seconds.")
 
     xpath, ypath = getPathCoords(bestPath, cities)
-    visualizePath(minTour, cities, xpath, ypath)
+    visualizePath(f"Brute Force - {str(minTour)[:8]} km", cities, xpath, ypath)
+
+
+    # WORST PATH
+    minTour, bestPath = bruteForceWorst(cities)
+    xpath, ypath = getPathCoords(bestPath, cities)
+    visualizePath(f"Brute Force (WORST) - {str(minTour)[:8]} km", cities, xpath, ypath)
